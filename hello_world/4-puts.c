ad33a2fc871640c2c3a23ec31a477c99d1d62dec
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-16 01:38:26 -0700

    command4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  *main - prints exactly "Programming is like building a multilingual puzzle,
+ *   *followed by a new line
+ *    *
+ *     *Return:  0 on success. Error code otherwise
+ *      */
+
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+	puts(str1);
+	return (0);
+}
