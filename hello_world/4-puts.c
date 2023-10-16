b4e1870301d755e28901f3d8e7b233de34483315
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-16 01:42:53 -0700

    command4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,11 +6,13 @@
  *   *followed by a new line
  *    *
  *     *Return:  0 on success. Error code otherwise
- *      */
+ *
+ */
 
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str1);
 	return (0);
 }
