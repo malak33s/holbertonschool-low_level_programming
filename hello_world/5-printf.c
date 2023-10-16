d6ee1293a2c77d36c6c1ced7de376e905f1f182c
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-16 02:00:00 -0700

    command5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-
+/**
+ * main - Print a string
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
