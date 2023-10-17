d28483a83cf93b1fc798ce4a28bff1d17f3bd4f5
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-17 05:51:03 -0700

    command2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints a random number and states whether it is positive
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{									putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
