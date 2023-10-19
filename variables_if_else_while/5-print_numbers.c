744f2e158ccc9eea776b1fda5f033c59b4d9514f
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-19 00:40:32 -0700

    command5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints all single numbers
+ *
+*Return: Always 0.
+ */
+int main(void)
+{
+int num = 0;
+
+	while (num < 10)
+{
+	printf("%i", num);
+	num++;
+}
+	printf("\n");
+	return (0);
+}
