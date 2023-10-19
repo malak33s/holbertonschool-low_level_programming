ffa389627195e5f56e262187d81f9d9dcfff1a4b
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-19 08:12:36 -0700

    command8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	char num;
 
-	for (num 'o'; num <= '9'; num++)
-	
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 	}
+
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
