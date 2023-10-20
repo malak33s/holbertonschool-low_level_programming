0f6ab6aab1bac5ab29014a16a5fd00d032bcf0d1
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-20 08:39:03 -0700

    command10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - adds two integers and returns the result
+ *@i1: first int
+ *@i2: second int
+ *
+ *Return: result
+ */
+int add(int i1, int i2)
+{
+int resultat;
+
+resultat = i1 + i2;
+return (resultat);
+
+}
