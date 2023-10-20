fbf4fc55cea81c52bbc43238124386b01e7a1c5f
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-20 07:40:02 -0700

    command9

---
+++ functions_nested_loops/9-time_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_tables - print the the 9 times table, starting with 0.
+ *
+ */
+void times_tables(void)
+{
+	int num1, num2, m;
+
+	for (num1 = 1; < 10 num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			m = num1 * num2;
+
+			if (num2 == 0)
+			{
+				_putchar(m + '0');
+			}
+			if (m < 10 &&  num2 != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m + '0');
+			}
+			else if (m >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+		}
+			_putchar('\n');
+	}
+}
