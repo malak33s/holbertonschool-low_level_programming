8676566fb1804c0e47019ed26f28433408e2951f
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-24 00:44:05 -0700

    command2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * strln - function returns the length or a string
+ * _strlen - function returns the length or a string
  *
- * int _strlen(char *s);
+ * @s: string to calculate the length
+ *
+ * Return: return the length of s
  */
 int _strlen(char *s)
 {
 	int length = 0;
+
 	while (s[length] != '\0')
 	{
 		length++;
