06be78adae10dc7628c62ec8bb1b50ba5cb59a7a
malak33s <melaksarsarhani1bga@gmail.com>
2023-10-31 02:46:28 -0700

    code2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: the string to analyse
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int lgt = 1;
+
+	if (*s == '\0')
+	{
+	return (0);
+	}
+
+	else
+	{
+	return (_strlen_recursion(s + 1) + lgt);
+	}
+}
