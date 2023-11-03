ac801a1a5e582bb21a9dbfd529154cf891a48fa3
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-03 08:12:15 -0700

    code4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - add two positive numbers
+ *
+ * @argc: int of arguments
+ * @argv: array of arguments
+ *
+ * Return: 1 if the number contains a non digit symbol,
+ * otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
