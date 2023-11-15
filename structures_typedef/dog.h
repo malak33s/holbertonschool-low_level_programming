546a535158c06bea87892cc0df2e23d181e0a861
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-15 04:19:45 -0800

    stuctures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * dog - Define a new type struct with the following element.
+ *
+ * @name: dog's name
+ * @age: age's
+ * @owner: owner's dog
+*/
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 #endif
