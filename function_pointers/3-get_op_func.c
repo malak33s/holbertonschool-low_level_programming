e6bce544c99d0c6ca854f37e5cb2abd1d824c9ab
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-17 05:12:09 -0800

    code3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,20 +1,75 @@
-#include"3-calc.h"
+#include "3-calc.h"
+
 /**
- * get_op_func -
- * @
- * Return:the result of opperation.
+ * op_add - returns the sum of 2 integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: sum of add.
  */
-int (*get_op_func(char *s))(int,int)
+
+int op_add(int a, int b)
 {
-	int i = 0;
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	while (ops[i].op!= NULL)
-	{
-		if(si
+	return (a + b);
+}
+
+
+/**
+ * op_sub - returns the difference betweens 2 integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: difference of substraction.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - returns the product of multiplication of 2 integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: product of multiplication.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - returns the result of division of 2 integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of division.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - returns the remainder of the division of 2 integers.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of modulo.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,28 +1,42 @@
 #include "3-calc.h"
-int main(int argc, char *argv [])
+
+/**
+ * main - main fonction.
+ *
+ * @argc: number of arguments.
+ * @argv: array.
+ *
+ * Return: int.
+ */
+
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char operator;
+	char *operator;
 
-	if (argc !=)
+	if (argc != 4)
 	{
-		print("Error\n");
+		printf("Error\n");
 		exit(98);
 	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	operator = argv[2];
-	
+
 	if (get_op_func(operator) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	if (((*operator) == '/' || (*operator) == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(operator)(num1, num2));
-}
 
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
+}
