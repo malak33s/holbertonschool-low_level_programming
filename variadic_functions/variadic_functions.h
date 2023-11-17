5a3afe75e5b0589a661297e9fa26bfa18f06304b
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-17 06:57:30 -0800

    code

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,9 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_name(char *name, void (*f)(char *));
+void print_all(const char * const format, ...);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
