033bd71bb91c2b2e86afa7b2a1ccd6916142e8cc
malak33s <melaksarsarhani1bga@gmail.com>
2023-11-17 08:13:49 -0800

    code

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,32 @@
 #include "main.h"
-
 /**
- * _strcmp - comparing strings
- * @s1 : var pointer
- * @s2 : pointer
- * Return: Always 0.
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ *
+ * Return: the number of bytes in the initial segment of @s which consist only
+ * of bytes from @accept
  */
-int _strcmp(char *s1, char *s2)
-{
-	int a;
 
-	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s1[a] != s2[a])
-			return (s1[a] - s2[a]);
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
 	}
-	return (0);
+	return (c);
 }
