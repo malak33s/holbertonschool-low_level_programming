b81cec26662b7b751735da293c25edf71fdea5c5
malak33s <melaksarhani1bga@gmail.com>
2024-02-19 14:31:13 -0800

    task0 betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -22,9 +22,9 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	else
-        {
-                printf("%d is zero\n", n);
-        }
+	{
+		printf("%d is zero\n", n);
+	}
 
 	return (0);
 }
