70d9c4a7137b4afcbfc2879d5d132ffa4fe0aa34
malak33s <melaksarhani1bga@gmail.com>
2024-02-19 11:37:29 -0800

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printf the string
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	printf("\"%s\"", "\n");
+
+	return (0);
+}
