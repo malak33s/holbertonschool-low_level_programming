5a6b8f7d68fd89da30a3d9a2ee0505e1f7740cf0
malak33s <melaksarhani1bga@gmail.com>
2024-02-20 07:37:44 -0800

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print all single numbers of base 10 staring from 0
+ *
+ * num: number
+ *Return: always 0
+ */
+
+int main(void)
+{
+        int num;
+
+        for (num = '0'; num <= '9'; num++)
+        {
+                putchar(num);
+        }
+        putchar('\n');
+        return (0);
+}
