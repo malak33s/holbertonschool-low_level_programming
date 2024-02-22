e38ca17926e114a875e6c5d0ac9c6606c4da51ac
mala33s <melaksarhani1bga@gmail.com>
2024-02-22 20:50:28 +0000

    task7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number.
+ *
+ *@lsd: last digit of a number.
+ *
+ *
+ * Return: The last digit of a number
+ *
+ *
+ * */
+int print_last_digit(int lsd)
+{
+	if (lsd >= 10)
+	
+	{
+		return (lsd);
+	}
+	return (lsd);
+}
