091fd27d5c41df086745bb6bdf8aff5c6558e64e
malak33s <melaksarhani1bga@gmail.com>
2024-02-23 14:14:00 +0000

    task9 correction de betty suspect code

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *times_table - print the 9 tables staring with 0.
  *
@@ -7,42 +8,39 @@
 void times_table(void)
 {
 	int num, sum;
-	int multi;
+	int fois;
 
 	for (num = 0; num <= 9; num++)
 	{
 		for (sum = 0; sum <= 9; sum++)
 		{
-			multi = num * sum;
-
+			fois = num * sum;
 			if (sum == 0)
 			{
-				if (num == 0)
+				_putchar(48);
+				if (sum < 9)
 				{
-					_putchar(48);
-				}
-				else
-				{
-					_putchar(48);
 					_putchar(',');
 					_putchar(' ');
 				}
 			}
+			else
+			{
+				if (fois < 10)
+				{
+					_putchar(' ');
+				}
 				else
+				{
+					_putchar((fois / 10) + 48);
+				}
+				_putchar((fois % 10) + 48);
+				if (sum < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
-
-					if (multi < 10)
-					{
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar((multi / 10) + 48);
-					}
-				_putchar((multi % 10) + 48);
 				}
+			}
 		}
 		_putchar('\n');
 	}
