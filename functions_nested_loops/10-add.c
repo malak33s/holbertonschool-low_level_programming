742000cd202962d95f05d2663f5d527d4b2cfd5c
malak33s <melaksarhani1bga@gmail.com>
2024-02-23 14:43:23 +0000

    task10 erreur de betty @ avant decla de variable

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * add - function that adds two integers.
+ *
+ *sum: sum of adds
+ *
+ *@num1: first integers.
+ *
+ *@num2: second integers.
+ * Return: the results
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	if (sum <= 48)
+	{
+		_putchar(sum);
+
+	}
+	return (sum);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,4 +12,5 @@ int _abs(int);
 int print_last_digit(int lsd);
 void jack_bauer(void);
 void times_table(void);
+int add(int num1, int num2);
 #endif
