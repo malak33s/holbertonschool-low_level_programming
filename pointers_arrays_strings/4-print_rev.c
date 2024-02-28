3fe421c0a889fe3bf39e5c8a07f40aab077d9216
malak33s <melaksarhani1bga@gmail.com>
2024-02-28 11:43:05 +0000

    reverse a string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - function for print a string in reverse.
+ * @s: pointeur to string.
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+	int temp;
+
+	for (temp = 0; s[temp]; temp++)
+
+	for (temp = temp - 1; temp >= 0; temp--)
+		_putchar(s[temp]);
+
+	_putchar('\n');
+}
