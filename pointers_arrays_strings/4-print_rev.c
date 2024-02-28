99430573d2ca998d94aae6cbc34000d979e1273e
malak33s <melaksarhani1bga@gmail.com>
2024-02-28 12:02:28 +0000

    reverse a string correction betty

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,8 @@ void print_rev(char *s)
 {
 	int temp;
 
-	for (temp = 0; s[temp]; temp++);
+	for (temp = 0; s[temp]; temp++)
+		;
 
 	for (temp = temp - 1; temp >= 0; temp--)
 		_putchar(s[temp]);
