1b6cf16279d13588d5bd637fee95595f39d2c939
malak33s <melaksarhani1bga@gmail.com>
2024-03-01 04:24:44 +0000

    last task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string
+ *@s: string
+ * Return: s's value
+*/
+
+char *leet(char *s)
+{
+	int a, b;
+
+	char *letters = "aeotlAEOTL";
+
+	char *numbers = "4307143071";
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; letters[b]; b++)
+		{
+			if (s[a] == letters[b])
+			{
+				s[a] = numbers[b]; 
+			}
+		}
+	}
+	return (s);
+}
