b4dcea775f2ae6b0bba610e89f876576217693fe
malak33s <melaksarhani1bga@gmail.com>
2024-04-07 20:45:44 +0000

    commile progrzm with my protot

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* list_lenn - write a function thaht count all element of a list
+* list_len - write a function thaht count all element of a list
 *@h: input for start count
 */
 
-size_t list_lenn(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t e = 0;
 
