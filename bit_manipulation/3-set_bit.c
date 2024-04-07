76a76210e8bf508486d76b6943dd9c114f9d9812
malak33s <melaksarhani1bga@gmail.com>
2024-04-07 18:56:15 +0000

    task 3 correction

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned lon4g int) * 8 || n == NULL)
+	if (index > sizeof(unsigned long int) * 8 || n == NULL)
 		return (-1);
 
 	*n |= (1 << index);
