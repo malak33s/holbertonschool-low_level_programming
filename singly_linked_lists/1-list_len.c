cc6cc439caa0ec55b6a97dcd60a25d6b40be78ba
malak33s <melaksarhani1bga@gmail.com>
2024-04-07 02:29:22 +0000

    correction betty in list.h

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,12 +7,12 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t elem = 0;
+	size_t elem = 0;
 
-    while (h != NULL)
-    {
-        elem++;
-        h = h->next;
-    }
-    return (elem);
+	while (h != NULL)
+	{
+		elem++;
+		h = h->next;
+	}
+	return (elem);
 }
