ae72a678fa55b12bb911cf39006fcfd97d798e73
malak33s <melaksarhani1bga@gmail.com>
2024-04-07 17:55:55 +0000

    readme, putchaar, mainh,

---
+++ bit_manipulation/README.md
@@ -0,0 +1,1 @@
+for the correction of my project but manipulation
---
+++ bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+#endif
